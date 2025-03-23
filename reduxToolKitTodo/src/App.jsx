f68bdbc12d280fbbd1fdd54ab616fdd4735bcbe0
ravikant-pandey <howtoearn554@gmail.com>
2025-03-23 22:05:25 +0530

    Todo with the help of Redux

--- reduxToolKitTodo/src/App.jsx
+++ reduxToolKitTodo/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
-
 import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 function App() {
+  
   return (
-   <>
-   <h1>Learn about Redux ToolKit</h1>
-   </>
+    <>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
+    </>
   )
 }
 
