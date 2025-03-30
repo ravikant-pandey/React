00dad6c7df47bb0f4cf1e0ede33393d2da600fce
ravikant-pandey <howtoearn554@gmail.com>
2025-03-31 00:56:50 +0530

    end of the day

--- megaBlogProject/src/App.jsx
+++ megaBlogProject/src/App.jsx
@@ -1,11 +1,42 @@
+import { useEffect, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
 import './App.css'
-
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 function App() {
-  return (
-    <>
-      <h1>Mega Blog App With Help Of AppWrite</h1>
-    </>
-  )
+
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .catch((error) => {
+        console.error('Error fetching user:', error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+    return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400 '>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO: {/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+    ):null
 }
 
 export default App
