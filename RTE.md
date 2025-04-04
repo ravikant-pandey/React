6845c7ca7f0bba8e91b0cc342d53a9d683bb64cc
ravikant-pandey <howtoearn554@gmail.com>
2025-04-04 21:23:30 +0530

    post form

---
+++ RTE.md
@@ -0,0 +1,19 @@
+<Editor
+            initialValue='default value'
+            init={
+                {
+                    branding: false,
+                    height: 500,
+                    manubar: true,
+                    plugins: [
+                        "advlist autolink lists link image charmap print preview anchor",
+                        "searchreplace visualblocks code fullscreen",
+                        "insertdatetime media table paste code help wordcount"
+                    ],
+                    toolbar:
+                        "undo redo | formatselect | bold italic backcolor | " +
+                        "alignleft aligncenter alignright alignjustify | " +
+                        "bullist numlist outdent indent | removeformat | " +
+                        "image media link | help"
+                }}
+        />
--- megaBlogProject/src/components/AuthLayout.jsx
+++ megaBlogProject/src/components/AuthLayout.jsx
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from 'react'
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux"
 import { useNavigate } from 'react-router-dom'
 // You can give different name of file and function name
 export default function Protected({ children, authentication = true }) {
     // This is a protected route, only accessible to authenticated users
+    const navigate = useNavigate()
+    const [loading, setLoading] = useState(true)
+    const authStatus = useSelector((state) => state.auth.status)
+    useEffect(() => {
+
+        //TODO: Make it more easy to understand
+
+        // if(authStatus===true)
+        // {
+        //     navigate('/')
+        // }
+        // else if(authStatus===false)
+        // {
+        //     navigate("/login")
+        // }
+
+        // let authVal = authStatus === true ? true : false
+        // true && (suppse authStatus = false so) false !== true = true (AND)
+        if (authentication && authStatus !== authentication) {
+            // If the user is not authenticated, redirect to the login page
+            navigate("/login")
+            // false && (true !== true means) false 
+        } else if (!authentication && authStatus !== authentication) {
+            // If the user is authenticated, redirect to the home page
+            navigate("/")
+        }
+        setLoading(false)
+    }, [authStatus, navigate, authentication])
     return (
-        <div>AuthLayout</div>
+        loading ? <h1>Loading</h1> : <>{children}</>
     )
 }
 
